4bf6fd7c86eddf17355f492d6487c82fb36e72e8
BobthebuilderORIGINAL <b.hadjiyankov2021@pmg-pernik.com>
2026-04-20 12:48:13 +0300

    contact

--- CarSimulatorApp/CarSimulatorApp/Controllers/HomeController.cs
+++ CarSimulatorApp/CarSimulatorApp/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace CarSimulatorApp.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
